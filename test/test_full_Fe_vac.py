c616dd7882d77cd5f4e1bcedfb8154a1c1e65504
Aldo <ealdoglielmo@gmail.com>
2018-04-18 19:25:06 +0100

    added a jupyter notebook with a performance test of theano and CasADi softwares for automatic differentiation

--- test/test_full_Fe_vac.py
+++ test/test_full_Fe_vac.py
@@ -1,8 +1,12 @@
+import os
 import logging
 
 import numpy as np
+import matplotlib.pyplot as plt
+from ase.io import read
 
 from original import better_MFF_database
+from original.better_MFF_database import carve_confs
 from original import Kernels
 from original import GP_for_MFF
 
@@ -12,20 +16,23 @@ logging.basicConfig(level=logging.INFO)
 # Parameters
 directory = 'data/Fe_vac/'
 r_cut = 4.45
-
+sigma = 0.4
+noise = 0.0001
+ntest = 200
 # ----------------------------------------
 # Construct a configuration database
 # ----------------------------------------
 
 if False:
 
-    n_data = 400
+    n_data = 5000
 
     filename = directory + 'movie.xyz'
 
     traj = read(filename, index=slice(None), format='extxyz')
 
-    elements, confs, forces, energies = carve_confs(traj, r_cut, n_data, USE_ASAP=False)
+    elements, confs, forces, energies = carve_confs(traj, r_cut, n_data,
+                                        forces_label= 'force', energy_label = 'energy', USE_ASAP=True)
 
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -47,10 +54,8 @@ if False:
 # Test a simple GP on the built database
 # ----------------------------------------
 
-if True:
+if False:
     # Parameters
-    sigma = 0.4
-    noise = 0.00001
     ntr = 5
     ntest = 50
 
@@ -90,6 +95,60 @@ if True:
     print(gp_forces)
     print('MAEF on forces: {:.4f} +- {:.4f}'.format(MAEF, SMAEF))
     print('Relative MAEF on forces: {:.4f} +- {:.4f}'.format(MAEF / MF, SMAEF / MF))
+
+# ----------------------------------------
+# Learning curve
+# ----------------------------------------
+
+if True:
+
+	ker = Kernels.TwoBodySingleSpecies(theta=[sigma, r_cut / 10.0, r_cut])
+	gp = GP_for_MFF.GaussianProcess(kernel=ker, noise=noise, optimizer=None)
+
+	ntrs = [10, 20, 50, 100]
+
+	errors = []
+
+	for ntr in ntrs:
+
+		# Get configurations and forces from file
+		confs = np.load(str(directory + 'confs_cut={:.2f}.npy'.format(r_cut)))
+		forces = np.load(str(directory + 'forces_cut={:.2f}.npy'.format(r_cut)))
+
+		numconfs = len(forces)
+		ind = np.arange(numconfs)
+		ind_tot = np.random.choice(ind, size = ntr + ntest, replace=False)
+
+		# Separate into testing and training dataset
+		tr_confs, tr_forces = confs[ind_tot[:ntr]], forces[ind_tot[:ntr]]
+		tst_confs, tst_forces = confs[ind_tot[ntr:]], forces[ind_tot[ntr:]]
+
+		print('Training GP')
+
+		gp.fit(tr_confs, tr_forces)
+
+		# Test the GP performance
+		print('Testing GP')
+
+		gp_forces = np.zeros((ntest, 3))
+		gp_error = np.zeros((ntest, 3))
+
+		for i in np.arange(ntest):
+			gp_forces[i, :] = gp.predict(np.reshape(tst_confs[i], (1, len(tst_confs[i]), 5)))
+			gp_error[i, :] = gp_forces[i, :] - tst_forces[i, :]
+
+		MAEF = np.mean(np.sqrt(np.sum(np.square(gp_error), axis=1)))
+		SMAEF = np.std(np.sqrt(np.sum(np.square(gp_error), axis=1)))
+		MF = np.mean(np.linalg.norm(tst_forces, axis=1))
+
+		print('MAEF on forces: {:.4f} +- {:.4f}'.format(MAEF, SMAEF))
+		print('Relative MAEF on forces: {:.4f} +- {:.4f}'.format(MAEF / MF, SMAEF / MF))
+
+		errors.append(MAEF / MF)
+
+	plt.plot(ntrs, errors)
+	plt.xscale('log')
+	plt.show()
 
 # ----------------------------------------
 # Map the GP on an M-FF

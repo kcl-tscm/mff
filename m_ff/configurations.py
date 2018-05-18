1d02704d31c5b461696dcca2d0e1703c9e0ea9b7
Adam Fekete <adam.fekete@kcl.ac.uk>
2018-05-18 19:22:09 +0100

    migrating crave_conf

--- m_ff/configurations.py
+++ m_ff/configurations.py
@@ -2,18 +2,103 @@ import logging
 from abc import ABCMeta, abstractmethod
 
 import numpy as np
-
 from asap3 import FullNeighborList
 
-# from ase.neighborlist import NeighborList
-
-
-USE_ASAP = True
 logger = logging.getLogger(__name__)
 
 
 class MissingData(Exception):
     pass
+
+
+def carve_from_snapshot(atoms, atoms_ind, r_cut, forces_label=None, energy_label=None):
+    # See if there are forces and energies, get them for the chosen atoms
+
+    forces = atoms.arrays.get(forces_label) if forces_label else atoms.get_forces()
+    energy = atoms.arrays.get(energy_label) if energy_label else atoms.get_potential_energy()
+
+    if forces is None and energy is None:
+        raise MissingData('Cannot find energy or force values in the xyz file, shutting down')
+
+    if forces is not None:
+        forces = forces[atoms_ind]
+    else:
+        logging.info('Forces in the xyz file are not present, or are not called force')
+
+    if energy is None:
+        logging.info('Energy in the xyz file is not present, or is not called energy')
+
+    # Build local configurations for every indexed atom
+    nl = FullNeighborList(r_cut, atoms=atoms)
+
+    confs = []
+    for i in atoms_ind:
+        indices, positions, distances = nl.get_neighbors(i)
+
+        atomic_numbers_i = np.ones((len(indices), 1)) * atoms.get_atomic_numbers()[i]
+        atomic_numbers_j = atoms.get_atomic_numbers()[indices].reshape(-1, 1)
+
+        confs.append(np.hstack([positions, atomic_numbers_i, atomic_numbers_j]))
+
+    return confs, forces, energy
+
+
+def carve_confs(atoms, r_cut, n_data, forces_label=None, energy_label=None):
+    confs, forces, energies = [], [], []
+
+    # Get the atomic number of each atom in the trajectory file
+    atom_number_list = [atom.get_atomic_numbers() for atom in atoms]
+    flat_atom_number = np.concatenate(atom_number_list)
+    elements, elements_count = np.unique(flat_atom_number, return_counts=True)
+
+    # Calculate the ratios of occurrence of central atoms based on their atomic number
+    ratios = np.sqrt(1.0 * elements_count) / np.sum(elements_count)
+    ratios /= np.sum(ratios)
+
+    if n_data > np.sum(elements_count):
+        print('WARNING: n_data is larger that total number of configuration')
+        n_data = np.sum(elements_count)
+        print('Settin n_data = {}'.format(n_data))
+
+    # Obtain the indices of the atoms we want in the final database from a linspace on the the flattened array
+    indices = [np.linspace(0, elc, int(ratio * n_data) - 1, dtype=np.int) for elc, ratio in zip(elements_count, ratios)]
+
+    # Go through each trajectory step and find where the chosen indexes for all different elements are
+    element_ind_count = np.zeros_like(elements)
+    element_ind_count_prev = np.zeros_like(elements)
+
+    for j in np.arange(len(atoms)):
+        logging.info('Reading traj step {}'.format(j))
+
+        this_ind = []
+
+        for k in np.arange(len(elements)):
+            count_el_atoms = sum(atom_number_list[j] == elements[k])
+            element_ind_count[k] += count_el_atoms
+            temp_ind = np.array([x for x in (indices[k] - element_ind_count_prev[k]) if (0 <= x < count_el_atoms)],
+                                dtype=np.int)
+
+            this_ind.append((np.where(atom_number_list[j] == elements[k]))[0][temp_ind])
+            element_ind_count_prev[k] += count_el_atoms
+
+        this_ind = np.concatenate(this_ind).ravel()
+
+        # Call the carve_from_snapshot function on the chosen atoms
+        if this_ind.size > 0:
+            this_conf, this_force, this_energy = \
+                carve_from_snapshot(atoms[j], this_ind, r_cut, forces_label, energy_label)
+            confs.append(this_conf)
+            forces.append(this_force)
+            energies.append(this_energy)
+
+    # Reshape everything so that confs is a list of numpy arrays, forces is a numpy array and energies is a numpy array
+    confs = [item for sublist in confs for item in sublist]
+    forces = [item for sublist in forces for item in sublist]
+
+    forces = np.asarray(forces)
+    energies = np.asarray(energies)
+
+    return elements, confs, forces, energies
 
 
 class Configurations(metaclass=ABCMeta):
@@ -79,7 +164,7 @@ class ConfsTwoBodySingleForces(Forces, SingleSpecies):
         np.save(filename_force, self.forces)
 
 
-class ConfsTwoForces(Forces, SingleSpecies ):
+class ConfsTwoForces(Forces, SingleSpecies):
     def __init__(self, r_cut):
         super().__init__()
 

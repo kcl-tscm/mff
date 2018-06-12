08e25604d98f57c2c53c263ca3053436ffbbbf27
Adam Fekete <adam.fekete@kcl.ac.uk>
2018-06-12 17:42:53 +0100

    Fixing setup process by installing subprojects as well

--- setup.py
+++ setup.py
@@ -1,27 +1,53 @@
+from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 
 
 tricube_cpp_module = Extension(
     'm_ff.interpolation.tricube_cpp._tricube',
-    sources=["m_ff/interpolation/tricube_cpp/tricube_module.c", "m_ff/interpolation/tricube_cpp/_tricube.c"]
+    sources=["m_ff/interpolation/tricube_cpp/tricube_module.c", "m_ff/interpolation/tricube_cpp/_tricube.c"],
 )
 
 tricube_fortran_module = Extension(
     'm_ff.interpolation.tricube_fortran._tricube',
-    sources=['m_ff/interpolation/tricube_fortran/_tricube.pyf', 'm_ff/interpolation/tricube_fortran/_tricube.c']
+    sources=['m_ff/interpolation/tricube_fortran/_tricube.pyf', 'm_ff/interpolation/tricube_fortran/_tricube.c'],
 )
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='m_ff',
+    name='m-ff',
     version='0.9.0',
-    description='This is a demo package',
     author=['Aldo Glielmo', 'Claudio Zeni', 'Adam Fekete'],
-    packages=['m_ff'],
-    # install_requires=['scipy', 'numpy', 'future'],
+    description='This is a demo package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kcl-tscm/M-FF",
+    packages=find_packages(),
     ext_modules=[tricube_cpp_module, tricube_fortran_module],
-    # ext_modules=[tricube_cpp_module],
     requires=['numpy']
+    # install_requires=['scipy', 'numpy', 'future'],
 )
 
 
-
+# import setuptools
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+#
+# setuptools.setup(
+#     name="example_pkg",
+#     version="0.0.1",
+#     author="Example Author",
+#     author_email="author@example.com",
+#     description="A small example package",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/pypa/example-project",
+#     packages=setuptools.find_packages(),
+#     classifiers=(
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ),
+# )

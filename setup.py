8d5a72c41b78ebcc4310db7e44f804e2a7d24f3a
Adam Fekete <adam.fekete@kcl.ac.uk>
2018-10-28 15:57:35 +0000

    changing the name in the setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages
 from numpy.distutils.core import setup, Extension
 
-
 tricube_cpp_module = Extension(
     'mff.interpolation.tricube_cpp._tricube',
     sources=["mff/interpolation/tricube_cpp/tricube_module.c", "mff/interpolation/tricube_cpp/_tricube.c"],
@@ -16,38 +15,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='m-ff',
+    name='mff',
     version='0.9.0',
-    author= 'Claudio Zeni, Adam Fekete, Aldo Glielmo',
+    author='Claudio Zeni, Adam Fekete, Aldo Glielmo',
     description='This is a demo package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kcl-tscm/M-FF",
+    url="https://github.com/kcl-tscm/mff",
     packages=find_packages(),
     ext_modules=[tricube_cpp_module, tricube_fortran_module],
     requires=['numpy', 'asap3', 'ase', 'theano', 'scipy']
-    # install_requires=['scipy', 'numpy', 'future'],
 )
-
-
-# import setuptools
-#
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-#
-# setuptools.setup(
-#     name="example_pkg",
-#     version="0.0.1",
-#     author="Example Author",
-#     author_email="author@example.com",
-#     description="A small example package",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/pypa/example-project",
-#     packages=setuptools.find_packages(),
-#     classifiers=(
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ),
-# )

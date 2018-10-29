d2dcc2878841b9296515c4333e842cb2069598aa
Adam Fekete <adam.fekete@kcl.ac.uk>
2018-10-29 01:29:06 +0000

    cleanup

--- original/cython_funcs/setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-setup(ext_modules = cythonize('Cython_kernels_cy.pyx'), include_dirs=[np.get_include()])
--- original/future/feature_extraction.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-# TODO: generating train and test datasets
-# TODO: sampling(weighted linspace, random)
-# TODO: sparse matrix representation of the input
-# TODO: application of sigmoid function

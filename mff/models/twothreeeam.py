790160f7ebccde443c7f91c7cf1123d1be74e757
Claudio Zeni <claudiopuntozeni@gmail.com>
2019-12-02 17:11:16 +0000

    Load bug #2

--- mff/models/twothreeeam.py
+++ mff/models/twothreeeam.py
@@ -1108,7 +1108,7 @@ class TwoThreeEamManySpeciesModel(Model):
             grid_filename_eam = 'GRID_ker_{p[gp_eam][kernel]}_ntr_{p[gp_eam][n_train]}.npz'.format(
                 p=params)
             print("Saved eam grid under name %s" % (grid_filename_eam))
-            params['grigrid_eamd']['filename'] = grid_filename_eam
+            params['grid_eam']['filename'] = grid_filename_eam
             self.grid_eam.save(path / grid_filename_eam)
 
         with open(path / "MODEL_23eam_ntr_{p[gp_2b][n_train]}.json".format(p=params), 'w') as fp:
@@ -1167,7 +1167,7 @@ class TwoThreeEamManySpeciesModel(Model):
         except:
             warnings.warn("The EAM GP file is missing")
             pass
-        
+
         if params['grid_2b']:
             for key, grid_filename_2b in params['grid_2b']['filename'].items():
                 k = tuple(int(ind) for ind in key.split('_'))

1c12ffe6f10c4b3f76ae2d77c045169c367b4afb
Claudio Zeni <claudiopuntozeni@gmail.com>
2019-12-30 13:39:39 +0000

    V 0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mff',
-    version='0.8',
+    version='0.9',
     author='Claudio Zeni, Adam Fekete, Aldo Glielmo',
     author_email='',
     description='Gaussian process regression to extract non-parametric 2-, 3- and many-body force fields.',

6346b112efb8579ce9129d851b6c744a5f70efb8
Claudio Zeni <claudiopuntozeni@gmail.com>
2020-01-27 14:19:10 +0000

    Bugfix
    Bugfix

--- mff/calculators.py
+++ mff/calculators.py
@@ -39,7 +39,7 @@ def eam_descriptor(dist, norm, rc, alpha, r0):
     try:
         dqdrij = -1/(2*q) * (dq1*q2 + q1*dq2)
     except ZeroDivisionError:
-        dqdrij = 0
+        dqdrij = np.zeros(len(q))
     dqdr = -dqdrij[:, None]*norm
     return q, dqdr
 
